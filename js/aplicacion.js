0ed07cdda32c5c7a625ab75296387e09b0baaeca
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-04-12 13:05:24 -0500

    Actualización de la dirección del servidor web

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,7 +1,7 @@
 //var server = 'http://192.168.0.111/justify/';
 //var server = 'http://localhost/justify/';
 //var server = 'http://panel.justify.com.mx/';
-var server = 'http://172.10.3.50/justify/';
+var server = 'http://192.168.2.4/justify/';
 
 var key_google = "AIzaSyAFa9dJnYA73gbJSdoery99NTOzUgbdL3w";
 
